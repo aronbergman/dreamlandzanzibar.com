0d31b43d9fddeb6e50c82a432cfd6fa7126e0c20
Aron Bergman <aron@bergman.re>
2023-11-21 20:22:53 +0100

    chore(ci/cd): update main.yml

--- pm2.config.js
+++ pm2.config.js
@@ -1,16 +1,16 @@
 module.exports = {
     apps: [
         {
-            name: 'dreamlandzanzibar.front',
-            script: 'node_modules/next/dist/bin/next',
-            args: 'start',
-            instances: 4,
-            exec_mode: 'cluster',
+            name: 'front.dreamlandzanzibar.com',
+            script : "npx",
+            // instances: 4,
+            exec_mode: 'fork',
             watch: false,
             max_memory_restart: '1000M',
             env_prod: {
                 NODE_ENV: 'production'
-            }
+            },
+            args: 'npx serve@latest out -p 3010',
         }
     ]
 }
